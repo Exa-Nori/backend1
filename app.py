943e352b15a9adaf397d8b3df40546d055842795
Better Name Pending <64957887+Exa-Nori@users.noreply.github.com>
2025-06-20 20:58:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,39 +6,26 @@ app = Flask(__name__, static_folder="public")
 @app.route("/")
 def index():
     return send_from_directory(app.static_folder, "index.html")
-import json
-from flask import Flask, request, jsonify
-import requests
-
-# Загрузка конфигурации из JSON
-with open('config.json') as config_file:
-    config = json.load(config_file)
-
-BOT_TOKEN = config['telegram_token']
-CHAT_ID = config['chat_id']
-
-app = Flask(__name__)
-
 @app.route('/api/send-to-telegram', methods=['POST'])
 def send_to_telegram():
+    bot_token = "6847127004:AAHJ8N5td3PAm40KJh2kY_2rMoCI72th4qg"
+    chat_id = "719874188"
+
     data = request.json
     if not data or 'name' not in data or 'message' not in data:
-        return jsonify({"error": "Поля name и message обязательны"}), 400
+        return jsonify({"error": "Пожалуйста, заполните name и message"}), 400
 
-    telegram_message = f"📝 Новое сообщение\n👤 Имя: {data['name']}\n💬 Сообщение: {data['message']}"
+    telegram_message = f"""
+    📝 Новое сообщение
+    👤 Имя: {data['name']}
+    💬 Сообщение: {data['message']}
+    """
     response = requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={"chat_id": CHAT_ID, "text": telegram_message}
+        f"https://api.telegram.org/bot{bot_token}/sendMessage",
+        json={"chat_id": chat_id, "text": telegram_message}
     )
+    return jsonify({"success": True}) if response.status_code == 200 else jsonify({"error": "Ошибка отправки!"}), 500
 
-    if response.status_code == 200:
-        return jsonify({"success": True}), 200
-    else:
-        return jsonify({"error": "Не удалось отправить сообщение"}), 500
-
-if __name__ == '__main__':
-    app.run()
-    
 from flask_compress import Compress
 
 Compress(app)
